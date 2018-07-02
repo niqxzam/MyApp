ded95f6e6566055e309ef3058332a8b57b69b74f
niqxzam <shah_nidz@yahoo.com>
2018-07-02 23:54:05 +0800

    Most things work just fine

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,11 +4,11 @@ class ListsController < ApplicationController
   # GET /Lists
   # GET /Lists.json
   def index
-    @Lists = List.all
+    @lists = List.all
       if params[:search]
-        @Lists = List.search(params[:search]).order("created_at DESC")
+        @lists = List.search(params[:search]).order("created_at DESC")
       else
-        @Lists = List.all.order("created_at DESC")
+        @lists = List.all.order("created_at DESC")
       end
   end
 
@@ -20,7 +20,7 @@ class ListsController < ApplicationController
 
   # GET /Lists/new
   def new
-    @List = List.new
+    @list = List.new
     respond_to do |format|
       format.html 
       format.js 
@@ -29,20 +29,20 @@ class ListsController < ApplicationController
 
   # GET /Lists/1/edit
   def edit
-    @List = List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   # POST /Lists
   # POST /Lists.json
   def create
-    @List = List.new(List_params)
+    @list = List.new(list_params)
     respond_to do |format|
-      if @List.save(validate: false)
-        format.html { redirect_to @List, notice: 'List was successfully created.' }
-        format.json { render :show, status: :created, location: @List }
+      if @list.save(validate: false)
+        format.html { redirect_to @list, notice: 'List was successfully created.' }
+        format.json { render :show, status: :created, location: @list }
       else
         format.html { render :new }
-        format.json { render json: @List.errors, status: :unprocessable_entity }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -50,14 +50,14 @@ class ListsController < ApplicationController
   # PATCH/PUT /Lists/1
   # PATCH/PUT /Lists/1.json
   def update
-    @List.update_attributes!(List_params)
+    @list.update_attributes!(list_params)
     respond_to do |format|
-      if @List.update(List_params)
-        format.html { redirect_to @List, notice: 'List was successfully updated.' }
+      if @list.update(list_params)
+        format.html { redirect_to @list, notice: 'List was successfully updated.' }
         format.json { render :show, status: :ok, location: @List }
       else
         format.html { render :edit }
-        format.json { render json: @List.errors, status: :unprocessable_entity }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -65,21 +65,21 @@ class ListsController < ApplicationController
   # DELETE /Lists/1
   # DELETE /Lists/1.json
   def destroy
-    @List.destroy
+    @list.destroy
     respond_to do |format|
-      format.html { redirect_to Lists_url, notice: 'List was successfully destroyed.' }
+      format.html { redirect_to lists_url, notice: 'List was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_List
-      @List = List.find(params[:id])
+    def set_list
+      @list = List.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def List_params
-      params.require(:List).permit(:title, :description, :image)
+    def list_params
+      params.require(:list).permit(:title, :description, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_150304) do
+ActiveRecord::Schema.define(version: 2018_07_02_151905) do
 
   create_table "authentications", force: :cascade do |t|
     t.string "uid"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 2018_07_02_150304) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "search_suggestions", force: :cascade do |t|
+    t.string "term"
+    t.integer "popularity"
   end
 
   create_table "users", force: :cascade do |t|
