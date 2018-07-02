59cd96609879c102c6c6bb70d1f537647daa9b11
niqxzam <shah_nidz@yahoo.com>
2018-07-02 23:15:26 +0800

    Pic upload works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_142607) do
+ActiveRecord::Schema.define(version: 2018_07_02_150304) do
+
+  create_table "authentications", force: :cascade do |t|
+    t.string "uid"
+    t.string "token"
+    t.string "provider"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
